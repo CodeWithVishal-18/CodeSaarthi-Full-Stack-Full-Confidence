b0d29df76625055760d1ca8ff9cd0847864092b8
Vishal Shukla <vishal.shuklaji45@gmail.com>
2026-05-23 14:35:25 +0530

    Remove Teacher's Section for Now

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -35,7 +35,7 @@ let Home = memo(() => {
             </div>
           </div>
         </div>
-        <div className="col-12 col-md-6 col-lg-3 mb-2">
+        {/* <div className="col-12 col-md-6 col-lg-3 mb-2">
           <div className="card h-100 shadow-sm" style={{ cursor: "pointer" }} onClick={() => navigate("/teachers")}>
             <div className="card-body">
               <div className="d-flex justify-content-between">
@@ -45,7 +45,7 @@ let Home = memo(() => {
               <p className="text-secondary">This section contains special notes shared by expert teachers. These notes are helpful for understanding important topics and preparing for interviews.</p>
             </div>
           </div>
-        </div>
+        </div> */}
       </div>
     </div >
   )
