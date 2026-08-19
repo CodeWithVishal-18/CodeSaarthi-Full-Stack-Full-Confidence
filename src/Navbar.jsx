338895316ad663e25bd19f4af4a60e8e36de3c3c
Vishal Shukla <vishal.shuklaji45@gmail.com>
2026-08-19 23:33:07 +0530

    Changes in Navbar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -9,7 +9,7 @@ let Navbar = memo(() => {
             <nav className="navbar navbar-expand-lg bg-secondary-subtle fixed-top">
                 <div className="container-fluid">
                     <Link className="navbar-brand" to={"/"}><span className='codeColorLogo'>Code</span><span className='saarthiColorLogo'>Saarthi</span></Link>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"  aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
